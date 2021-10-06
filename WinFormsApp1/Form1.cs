bf83527119007689f53f16f66c6a29472b911bf8
varax <varax@DESKTOP-CH496DU>
2021-10-06 18:55:13 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[,] mass = { { "adm", "adm" }, { "kik", "kik" }, { "varax", "rur" }, { "jojo", "top" }, { "kick", "butovski" } };
+            int a = 0;
+            for (int i = 0; i <= 4; i++)
+            {
+                
+                if (textBox1.Text == mass[i, 0] && textBox2.Text == mass[i, 1])
+                {
+                    a = i + 2;
+                    MessageBox.Show("U`re welcome!");
+                    break;
+                }
+                
+
+            }
+
+            if (a < 1)
+            {
+                MessageBox.Show("Incorrect login/password. Try again!");
+            }
+
+
+
+        }
+    }
+}
